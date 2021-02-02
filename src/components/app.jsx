6247f4ca87bd99fe174316133d605c546d2a9c27
JordanBrooks22 <73143990+JordanBrooks22@users.noreply.github.com>
2021-02-01 23:02:42 -0500

    added componentes

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import NavigationBar from './NavigationBar/navigationBar';
-import BookingPage from './BookingPage/bookingpage';
+
 
 class App extends Component {
     state = {  }
@@ -13,16 +13,6 @@ class App extends Component {
         );
     }
 }
-class Bravo extends Component {
-    state = { }
-    render() {
-        return (
-            <div className='container-fluid'>
-                <BookingPage />
-            </div>
-        )
-    }
-}
 
 export default App;
-export default Bravo;
+
